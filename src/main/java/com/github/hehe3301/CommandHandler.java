d4878810e761057b2472ec61a69bb3249e8970e9
Alexander H <hehe3301@gmail.com>
2018-10-07 10:23:07 -0400

    Removal of Extra commands from source bot

--- src/main/java/com/github/hehe3301/CommandHandler.java
+++ src/main/java/com/github/hehe3301/CommandHandler.java
@@ -57,69 +57,7 @@ public class CommandHandler {
             if(botVoiceChannel == null)
                 return;
 
-            TrackScheduler scheduler = getGuildAudioPlayer(event.getGuild()).getScheduler();
-            scheduler.getQueue().clear();
-            scheduler.nextTrack();
-
             botVoiceChannel.leave();
-
-        });
-
-        // Plays the first song found containing the first arg
-        commandMap.put("playsong", (event, args) -> {
-
-            IVoiceChannel botVoiceChannel = event.getClient().getOurUser().getVoiceStateForGuild(event.getGuild()).getChannel();
-
-            if(botVoiceChannel == null) {
-                BotUtils.sendMessage(event.getChannel(), "Not in a voice channel, join one and then use joinvoice");
-                return;
-            }
-
-            // Turn the args back into a string separated by space
-            String searchStr = String.join(" ", args);
-
-            loadAndPlay(event.getChannel(), searchStr);
-
-
-        });
-
-        // Skips the current song
-        commandMap.put("skipsong", (event, args) -> {
-
-            skipTrack(event.getChannel());
-
-        });
-
-        commandMap.put("exampleembed", (event, args) -> {
-
-            EmbedBuilder builder = new EmbedBuilder();
-
-            builder.appendField("fieldTitleInline", "fieldContentInline", true);
-            builder.appendField("fieldTitleInline2", "fieldContentInline2", true);
-            builder.appendField("fieldTitleNotInline", "fieldContentNotInline", false);
-            builder.appendField(":tada: fieldWithCoolThings :tada:", "[hiddenLink](http://i.imgur.com/Y9utuDe.png)", false);
-
-            builder.withAuthorName("authorName");
-            builder.withAuthorIcon("http://i.imgur.com/PB0Soqj.png");
-            builder.withAuthorUrl("http://i.imgur.com/oPvYFj3.png");
-
-            builder.withColor(255, 0, 0);
-            builder.withDesc("withDesc");
-            builder.withDescription("withDescription");
-            builder.withTitle("withTitle");
-            builder.withTimestamp(100);
-            builder.withUrl("http://i.imgur.com/IrEVKQq.png");
-            builder.withImage("http://i.imgur.com/agsp5Re.png");
-
-            builder.withFooterIcon("http://i.imgur.com/Ch0wy1e.png");
-            builder.withFooterText("footerText");
-            builder.withFooterIcon("http://i.imgur.com/TELh8OT.png");
-            builder.withThumbnail("http://www.gstatic.com/webp/gallery/1.webp");
-
-            builder.appendDesc(" + appendDesc");
-            builder.appendDescription(" + appendDescription");
-
-            RequestBuffer.request(() -> event.getChannel().sendMessage(builder.build()));
 
         });
 
