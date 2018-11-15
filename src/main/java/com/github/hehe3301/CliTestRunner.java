d95609a885bf6a59ae7b4fc9e0d9cb46e047a2cf
nupanick <nupanick@gmail.com>
2018-11-14 21:51:11 -0800

    Got the mock client sorta working. Can at least get to the login() method now.

--- src/main/java/com/github/hehe3301/CliTestRunner.java
+++ src/main/java/com/github/hehe3301/CliTestRunner.java
@@ -1,9 +1,22 @@
 package com.github.hehe3301;
+import com.github.hehe3301.bot.BotUtils;
+import com.github.hehe3301.bot.CommandHandler;
+import com.github.hehe3301.test.MockDiscordClient;
+import sx.blah.discord.api.ClientBuilder;
+import sx.blah.discord.api.IDiscordClient;
+
 import java.util.Scanner;
 
 public class CliTestRunner {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        CommandHandler mockBot = new CommandHandler();
+
+        IDiscordClient client = new MockDiscordClient();
+        client.getDispatcher().registerListener(new CommandHandler());
+        client.login();
+
+
         System.out.print("Enter a name: ");
         String name = in.nextLine().trim();
         String greeting = "Hello, %s!";
