45f73c862b8448f2f8be7c09b2b6612100175cfc
nupanick <nupanick@gmail.com>
2018-11-15 17:52:17 -0800

    Clean up duplicated logic in MainRunner.

--- src/main/java/com/github/hehe3301/MainRunner.java
+++ src/main/java/com/github/hehe3301/MainRunner.java
@@ -3,32 +3,30 @@ package com.github.hehe3301;
 import com.github.hehe3301.bot.BotUtils;
 import com.github.hehe3301.bot.CommandHandler;
 import com.github.hehe3301.configs.Secrets;
+import com.github.hehe3301.time_handler.TimeHandler;
 import sx.blah.discord.api.IDiscordClient;
 
 public class MainRunner {
 
     public static void main(String[] args){
 
-        if(args.length != 1){
+        String authToken;
+        if(args.length != 1) {
             System.out.println("Please enter the bots token as the first argument e.g java -jar thisjar.jar tokenhere");
-            IDiscordClient cli = BotUtils.getBuiltDiscordClient(Secrets.getInstance().BOT_TOKEN);
-
-            // Register a listener via the EventSubscriber annotation which allows for organisation and delegation of events
-            cli.getDispatcher().registerListener(new CommandHandler());
-
-            // Only login after all events are registered otherwise some may be missed.
-            cli.login();
-            return;
+            authToken = Secrets.BOT_TOKEN;
+        } else {
+            authToken = args[0];
         }
 
-        IDiscordClient cli = BotUtils.getBuiltDiscordClient(args[0]);
+        IDiscordClient cli = BotUtils.getBuiltDiscordClient(authToken);
 
         // Register a listener via the EventSubscriber annotation which allows for organisation and delegation of events
-        cli.getDispatcher().registerListener(new CommandHandler());
+        TimeHandler th = new TimeHandler();
+        CommandHandler ch = new CommandHandler(th);
+        cli.getDispatcher().registerListener(ch);
 
         // Only login after all events are registered otherwise some may be missed.
         cli.login();
-
     }
 
 }
